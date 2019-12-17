92f91b0ad9771f7a3c1aebd7ae133cb161a7d4de
Данил Черкашин <teddycomrad21@gmail.com>
2019-12-17 20:36:31 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+(function ($) {
+    var slides = $('.slide');
+    var prev = $('.prev');
+    var next = $('.next');
+    
+    function nextSlide() {
+        var slideActive = $('.slide.active');
+        
+        slideActive.toggleClass('active');
+
+        if (slideActive.next().length > 0) {
+            slideActive.next().toggleClass('active');
+        } else {
+            slides.first().toggleClass('active');
+        }
+    }
+
+    function prevSlide() {
+        var slideActive = $('.slide.active');
+
+        slideActive.toggleClass('active');
+
+        if (slideActive.prev().length > 0) {
+            slideActive.prev().toggleClass('active');
+        } else {
+            slides.last().toggleClass('active');
+        }
+        
+    }
+
+    setInterval(() => {
+        nextSlide();
+    }, 1500);
+
+    next.click(function() {
+        nextSlide();
+    });
+
+    prev.click(function() {
+        prevSlide();
+    });
+
+})(jQuery);
